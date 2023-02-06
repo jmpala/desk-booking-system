55c2af7e4cb823e672e9a7d550ae964dfb052b17
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-02-06 15:04:06 +0100

    close #5
    - Doctrine
    - Make
    - StofDoctrineExtensionsBundle

---
+++ migrations/Version20230206135001.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ * @codeCoverageIgnore
+ */
+final class Version20230206135001 extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Test migration';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE test_task (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE test_task');
+    }
+}
