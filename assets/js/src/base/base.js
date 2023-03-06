063ec3d335e0fd41b5249b807094fb0c1c0cade3
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-03-06 14:46:04 +0100

    on Base twig template - navbar now is not more sticky - added the container class to the content
    on Overview twig template
    - container css class is now implemented on base twig
    - added the Seatmap
    - added missing js, css and controller
    
    Other:
    - added missing base.js

---
+++ assets/js/src/base/base.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+import '../../../sass/pages/main/main.scss';
---
+++ src/Controller/OfficeOverviewController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OfficeOverviewController extends AbstractController
+{
+    #[Route('/', name: 'app_office_overview')]
+    public function index(): Response
+    {
+        return $this->render('office_overview/index.html.twig', [
+            'controller_name' => 'OfficeOverviewController',
+        ]);
+    }
+}
