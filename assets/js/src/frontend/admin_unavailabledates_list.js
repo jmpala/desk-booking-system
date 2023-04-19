7a206fb01bf74caadda85f053ca0a0152631b009
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-04-19 11:57:38 +0200

    Fixed broken functionality - see past unavailable dates - order the table

--- assets/js/src/frontend/admin_unavailabledates_list.js
+++ assets/js/src/frontend/admin_unavailabledates_list.js
@@ -8,11 +8,24 @@ const currentPage = window.location.pathname;
   console.log('admin_unavailabledates_list.js loaded');
 
   const table = document.querySelector('#unavailableDatesTable');
+  const selectedColumn: HTMLElement = document.querySelector('[data-col-selected="true"]');
+  const iconSelected: HTMLElement = selectedColumn.querySelector('i');
+  const pastCheck: HTMLInputElement = document.getElementById('pastUnavailabledates');
+
   const deleteForm: HTMLFormElement = document.querySelector('form[name="delete_unavailable_dates"]');
   const deleteModal: HTMLElement = document.querySelector('#deleteConfirmationModal');
   const idToDeleteLabel: HTMLElement = document.querySelector('#idToDeleteLabel');
 
+  iconSelected.classList.remove('bi-arrow-down-up');
+  if (selectedColumn.dataset.colOrder === 'asc') {
+    iconSelected.classList.add('bi-arrow-down');
+  } else {
+    iconSelected.classList.add('bi-arrow-up');
+  }
+
+  table.addEventListener('click', orderTable);
   table.addEventListener('click', editButtonClick);
+  pastCheck.addEventListener('change', onCheckedPastBookings);
   deleteModal.addEventListener('show.bs.modal', prepareFormAction);
 
   function editButtonClick(event) {
@@ -28,4 +41,49 @@ const currentPage = window.location.pathname;
     idToDeleteLabel.innerText = unavailableDatesId;
     deleteForm.action = `/admin/unavailableDates/${unavailableDatesId}/delete`;
   }
+
+  function orderTable(event: Event): void {
+    const target: HTMLElement = event.target;
+
+    if (!target.classList.contains('bi')
+      && !target.classList.contains('order-btn')) return;
+
+    const parentColumn: HTMLElement = target.closest('th');
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page') || '1';
+
+    const col = parentColumn.dataset.colName;
+    const order = parentColumn.dataset.colOrder === 'asc' ? 'desc' : 'asc';
+
+    let newUri = `${window.location.origin}${window.location.pathname}?page=${page}&col=${col}&ord=${order}`;
+
+    const past = document.querySelector('[data-past-bookings]');
+    if (pastCheck.checked) {
+      newUri += '&past=true';
+    }
+
+    window.location.href = newUri;
+  }
+
+  function onCheckedPastBookings(event: Event): void {
+    const target: HTMLInputElement = event.target;
+
+    if (target.id !== 'pastUnavailabledates'
+      && target.id !== 'checkForPastBookings') return;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page') || '1';
+    const col = urlParams.get('col') || 'resource';
+    const order = urlParams.get('ord') || 'asc';
+
+    let newUri = `${window.location.origin}${window.location.pathname}?page=${page}&col=${col}&ord=${order}`;
+
+    if (target.checked) {
+      newUri += '&past=true';
+    }
+
+    window.location.href = newUri;
+  }
+
 })();
