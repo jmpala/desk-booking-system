1bb88e669f6f9b10f0ab04e4730cea53c728e017
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-05-16 13:49:59 +0200

    Bookable places with their respective lat. and lon. are hardcoded on the map

--- assets/components/office-map/index.vue
+++ assets/components/office-map/index.vue
@@ -1,29 +1,47 @@
 <template>
-    <l-map
-        ref="map"
-        style="height: 350px"
-        :max-bounds="mapBoundaries"
-        :zoom="zoom"
-        :min-zoom="minZoom"
-        :max-zoom="maxZoom"
-        :center="center"
-        :crs="crs"
-    >
-        <l-image-overlay
-            :url="url"
-            :bounds="bounds"
-        />
-    </l-map>
+    <div>
+        <l-map
+            ref="map"
+            style="height: 350px"
+            :max-bounds="mapBoundaries"
+            :zoom="zoom"
+            :min-zoom="minZoom"
+            :max-zoom="maxZoom"
+            :center="center"
+            :crs="crs"
+            @mousemove="handleMouseMove"
+        >
+            <l-image-overlay
+                :url="url"
+                :bounds="bounds"
+            />
+
+            <l-marker
+                v-for="b in bookables"
+                :lat-lng="[b.lat, b.lon]"
+                :key="b.id"
+            >
+                <l-icon
+                    :icon-url="'./images/available.png'"
+                    :icon-size="[30, 30]"
+                />
+            </l-marker>
+
+        </l-map>
+        <p>Lat:{{ mouseLat }} Long:{{ mouseLon }}</p>
+    </div>
 </template>
 
 <script>
 import { CRS } from 'leaflet';
-import { LMap, LImageOverlay } from 'vue2-leaflet';
+import { LMap, LImageOverlay, LMarker, LIcon } from 'vue2-leaflet';
 
 export default {
     components: {
         LMap,
         LImageOverlay,
+        LMarker,
+        LIcon,
     },
     data() {
         return {
@@ -35,8 +53,93 @@ export default {
             zoom: -1,
             maxZoom: 0,
             crs: CRS.Simple,
+            bookables: [
+                { // left down corner
+                    id: 9991,
+                    lat: 0,
+                    lon: 0,
+                },
+                { // right upper corner
+                    id: 9992,
+                    lat: 1095,
+                    lon: 1899,
+                },
+                {
+                    id: 1,
+                    lat: 918,
+                    lon: 180,
+                },
+                {
+                    id: 2,
+                    lat: 918,
+                    lon: 419,
+                },
+                {
+                    id: 3,
+                    lat: 918,
+                    lon: 613,
+                },
+                {
+                    id: 4,
+                    lat: 918,
+                    lon: 1243,
+                },
+                {
+                    id: 5,
+                    lat: 918,
+                    lon: 1474,
+                },
+                {
+                    id: 6,
+                    lat: 918,
+                    lon: 1673,
+                },
+                {
+                    id: 7,
+                    lat: 640,
+                    lon: 180,
+                },
+                {
+                    id: 8,
+                    lat: 640,
+                    lon: 419,
+                },
+                {
+                    id: 9,
+                    lat: 640,
+                    lon: 1243,
+                },
+                {
+                    id: 10,
+                    lat: 640,
+                    lon: 1474,
+                },
+                {
+                    id: 11,
+                    lat: 640,
+                    lon: 1673,
+                },
+                {
+                    id: 12,
+                    lat: 490,
+                    lon: 280,
+                },
+                {
+                    id: 13,
+                    lat: 462,
+                    lon: 490,
+                },
+            ],
+            mouseLat: 0,
+            mouseLon: 0,
         };
-    }
+    },
+    methods: {
+        handleMouseMove(event) {
+            this.mouseLat = event.latlng.lat;
+            this.mouseLon = event.latlng.lng;
+        },
+    },
 };
 </script>
 
